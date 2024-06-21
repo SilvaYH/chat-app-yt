a27f7af2c9bb34baa9294cab93fd08a2b3940cc3
silva <443904591@qq.com>
2024-06-21 14:31:46 +0800

    build

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import path from 'path'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 import authRoutes from './routes/auth.routes.js'
@@ -10,18 +11,22 @@ import { app, server } from './socket/socket.js'
 dotenv.config()
 
 const port = process.env.PORT || 5500
+const __dirname = path.resolve()
 
+app.use(express.json())
+app.use(cookieParser())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// routes
+app.use('/api/auth', authRoutes)
+app.use('/api/message', messageRoutes)
+app.use('/api/users', userRoutes)
+// router end
+app.use(express.static(path.join(__dirname, '/frontend/dist')))
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
+})
 server.listen(port, () => {
 	connectToMongoDB()
 	console.log(`Example app listening on port ${port}`)
 })
-app.use(express.json())
-app.use(cookieParser())
-app.use(bodyParser.urlencoded({ extended: true }))
-// routes
-app.get('/', (req, res) => {
-	res.send('Hello World!')
-})
-app.use('/api/auth', authRoutes)
-app.use('/api/message', messageRoutes)
-app.use('/api/users', userRoutes)
